95754dc8eb4bb267ddfecff13f3a81d99037bc5e
Artem <56774094+FokuZz@users.noreply.github.com>
2019-10-20 12:55:20 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+   $("#phone").mask("+7 (999) 999-99-99");
+   
+});
+
+
+function show ()
+{
+var elem = document.getElementById()
+if (elem)
+elem.style.display="block";
+}
+
+function viewdiv(id){
+var el=document.getElementById(id);
+if(el.style.display=="block"){
+el.style.display="none";
+} else {
+el.style.display="block";
+}
+}
